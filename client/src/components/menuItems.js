1f1a7749dd2d35f631f4362146079f07663391a6
Ausaf1 <ausaf.esrar@gmail.com>
2022-07-05 12:20:35 +0530

    menuItem

---
+++ client/src/components/menuItems.js
@@ -0,0 +1,20 @@
+export const menuItems = [
+    {
+        title: "Home"
+    },
+    {
+        title: "AboutUS"
+    },
+    {
+        title: "Services"
+    },
+    {
+        title: "Important Documents"
+    },
+    {
+        title: "Institute Login"
+    },
+    {
+        title: "Student Login"
+    }
+];
